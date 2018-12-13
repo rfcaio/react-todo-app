653434713cc3168d64693845baea5642b66bd905
Caio Rodrigues <rfcaio90@gmail.com>
2018-12-13 13:34:54 -0300

    update dev dependencies

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 class Todo extends React.Component {
   render () {
     return (
-      <main>
+      <main className="container">
         <h1>Todo App</h1>
       </main>
     )
