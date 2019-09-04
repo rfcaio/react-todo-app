b069782564006a44a35f075ddd64c4e69ff2ae8f
Caio Rodrigues <rfcaio90@gmail.com>
2019-09-04 17:31:28 -0300

    update devDependencies and change webpack config file structure

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,30 +1,36 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge')
 const path = require('path')
 
-module.exports = {
-  devServer: {
-    contentBase: './dist',
-    inline: true,
-    overlay: true,
-    port: 8001
-  },
-  devtool: 'inline-source-map',
-  entry: './src/index.js',
-  mode: 'development',
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
-      { exclude: /node_modules/, test: /\.js$/, use: ['babel-loader'] }
-    ]
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, './dist')
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'src/index.html'
-    })
-  ],
-  watch: true
-}
+const commonConfig = merge([
+  {
+    devServer: {
+      contentBase: './dist',
+      inline: true,
+      overlay: true,
+      port: 8001
+    },
+    devtool: 'inline-source-map',
+    entry: './src/index.js',
+    module: {
+      rules: [
+        { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+        { exclude: /node_modules/, test: /\.js$/, use: ['babel-loader'] }
+      ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, './dist')
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: 'src/index.html'
+      })
+    ],
+    watch: true
+  }
+])
+
+const developmentConfig = merge([])
+
+module.exports = mode => merge(commonConfig, developmentConfig, { mode })
