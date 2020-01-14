a93917c8e3fbd5fe8f6b5cb31cfb563889faba53
Caio Rodrigues <rfcaio90@gmail.com>
2020-01-14 08:56:03 -0300

    remove unnecessary cleanup

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { cleanup, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 
 import App from './App'
 
 describe('App', () => {
-  afterEach(cleanup)
-
   describe('on render', () => {
     test('show `Todo` component', () => {
       const { getByTestId } = render(<App />)
